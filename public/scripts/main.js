5250b8d60bfed0b60d4144bd2aeb2e1057a1bd82
hnrchrdl <hinrich@posteo.de>
2017-11-10 22:54:22 +0100

    good weather bad weather

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,24 +1,54 @@
-
-
 (function() {
-    var isSlided = false;
+    var goodWeather = false;
     var ws = new WebSocket("wss://websocket-service.herokuapp.com/");
     ws.onmessage = function(msg) {
-        var txt = JSON.parse(msg.data).message;
+        
+        console.log(msg)
+
+        var msgObj = JSON.parse(msg.data);
+        
+        var txt = msgObj.message;
+        // var score = msgObj.score;
         updateMsg(txt);
+        // updateMsg(txt + score ? " " + score.toString(): "");
     }
     window.onload = function(){
 
-        // document.onclick = function(event) {    
-        //     if(!isSlided) {
-        //         startSlide();
-        //     }
-        // };
+        var urlParams = new URLSearchParams(window.location.search);
+        goodWeather = urlParams.has("weather") ? 
+            urlParams.get("weather") === 'good':
+            false;
+        
+        var cover = document.getElementById("coverweather");
+        cover.classList.remove("bad");
+        cover.classList.remove("good");
+        cover.className += goodWeather ? " good" : " bad";
+        
+        if (goodWeather) {
+            if (goodWeather) {
+                var sun = document.getElementById("bigsun");
+                sun.className += " hide";
+    
+                var man = document.getElementById("sunny-man");
+                man.className += " hide"
+            }
+        }
+
+        // set headline text
+        var headline = document.getElementById("headline-text");
+        headline.innerHTML = goodWeather ?
+        'TODAY IS SUNNY <span><object class="sun-small" data="/images/small_sun.svg" type="image/svg+xml"></object></span> JUST GO ENJOY HAMBURG!':
+        "LOOKING FOR THE SUN?";
+        
+        // initialize
+        cover.classList.remove("hide");
+        var cover2 = document.getElementById("covernews");
+        cover2.classList.remove("hide")
+        
 
         setTimeout(function() {
-            if(!isSlided) {
-                startMessages();
-            }
+            
+            startMessages();
 
             setInterval(function(){
                 console.log('stop')
@@ -27,7 +57,7 @@
                     console.log('start')
                     startMessages();
                  }, 5000);
-            }, 30000);
+            }, 15000);
 
         }, 5000)
 
@@ -36,35 +66,39 @@
 
     var startMessages = function() {
 
-        var cover = document.getElementById("coverbadwheather");
+        var cover = document.getElementById("coverweather")
         cover.className += " fade-out"
-        
-        var sun = document.getElementById("bigsun");
-        sun.className += " fade-out-up";
-
-        var man = document.getElementById("sunny-man");
-        man.className += " fade-out-down"
 
         var headline = document.getElementById("headline");
         headline.className += " fade-out-up";
 
+        if (!goodWeather) {
+            var sun = document.getElementById("bigsun");
+            sun.className += " fade-out-up";
+
+            var man = document.getElementById("sunny-man");
+            man.className += " fade-out-down"
+        }
+
         var gn = document.getElementById('good-news-container');
         gn.className += " fade-in-up";
+
     }
 
-
     var stopMessages = function() {
-        var cover = document.getElementById("coverbadwheather");
-        cover.classList.remove("fade-out");
-        
-        var sun = document.getElementById("bigsun");
-        sun.classList.remove("fade-out-up");
-
-        var man = document.getElementById("sunny-man");
-        man.classList.remove("fade-out-down");
-
+        var cover = document.getElementById("coverweather");
+            cover.classList.remove("fade-out");
+            
         var headline = document.getElementById("headline");
         headline.classList.remove("fade-out-up");
+        
+        if (!goodWeather) {
+            var sun = document.getElementById("bigsun");
+            sun.classList.remove("fade-out-up");
+    
+            var man = document.getElementById("sunny-man");
+            man.classList.remove("fade-out-down");
+        }
 
         var gn = document.getElementById('good-news-container');
         gn.className += " fade-in-up";
